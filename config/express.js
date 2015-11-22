d82fa7f53946259f7fb45bfae593339c8cd6efcc
MrLokans <trikster1911@gmail.com>
2015-11-22 12:21:51 +0300

    Configuration enhancements

--- config/express.js
+++ config/express.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var session = require('express-session');
+var flash = require('connect-flash');
+var cookieSession = require('cookie-session')
 
+var mongoStore = require('connect-mongo')(session);
 var path = require('path');
 
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var passport = require('passport');
+
+var config = require('./config');
+var pkg = require('../package.json');
 
 module.exports = function(app, passport){
     app.set('views', path.join(__dirname, '..', 'views'));
@@ -15,7 +21,20 @@ module.exports = function(app, passport){
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
     app.use(session({secret: 'SECRET'}));
+    app.use(cookieSession({ secret: 'secret' }));
+    app.use(session({
+        resave: true,
+        saveUninitialized: true,
+        secret: pkg.name,
+        store: new mongoStore({
+            url: config.db,
+            collection : 'sessions'
+        })
+    }));
     app.use(passport.initialize());
     app.use(passport.session());
+
+    // connect flash for flash messages - should be declared after sessions
+    app.use(flash());
     app.use(express.static(path.join(__dirname, '..', 'public')));
 };
