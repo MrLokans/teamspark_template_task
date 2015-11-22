07dd7c4e073410e95637bf51917b34b825a7a6cc
MrLokans <trikster1911@gmail.com>
2015-11-22 10:25:49 +0300

    Post model enhanced.

--- models/Posts.js
+++ models/Posts.js
@@ -6,7 +6,44 @@ var PostSchema = new mongoose.Schema({
     link: String,
     upvotes: {type: Number, default: 0},
     date: { type: Date, default: Date.now },
-    comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
+    comments: [{
+        body: {type: String, default: ''},
+        user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        date: {type: Date, default: Date.now }
+    }]
 });
+
+PostSchema.methods = {
+    addComment: function(user, comment, cb){
+        this.comments.push({
+            body: comment.body,
+            user: user._id,
+        });
+
+        this.save(cb);
+    },
+
+    removeComment: function(commentId, cb){
+        // think about removing the comment
+    }
+};
+
+PostSchema.statics = {
+    load: function(id, cb){
+        this.findOne({ _id: id })
+            .populate('user', 'name email username')
+            .poluate('comments.user')
+            .exec(cb);
+    },
+
+    list: function(options, cb){
+        var criteria = options.criteria || {};
+
+        this.find(criteria)
+            .populate('user', 'name username')
+            .sort({'date': -1})
+            .exec(cb);
+    }
+};
 
 mongoose.model('Post', PostSchema);
